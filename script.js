c03b8f300d84b87330da03bc1ed69953d63d19a8
Highbrook <Milivojevicdjole@yahoo.com>
2022-05-27 18:44:19 +0200

    added gradient changer functionality

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 const cpOne = document.querySelector('#cpOne');
 const cpTwo = document.querySelector('#cpTwo');
+const html = document.querySelector('html');
 
-console.log(cpOne.value, cpTwo.value);
+cpOne.addEventListener('change', colorGrab);
 
-cpOne.addEventListener('change', () => {
-    console.log("Color changed");
-});
+cpTwo.addEventListener('change', colorGrab);
 
-cpTwo.addEventListener('change', () => {
-    console.log("Color changed");
-});
+function colorGrab() {
+    html.style.background = `linear-gradient(${cpOne.value}, ${cpTwo.value})`;
+    console.log(cpOne.value, cpTwo.value);
+}
+
