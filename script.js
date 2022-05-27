1903f2fe43105c47ef9caeba32bbd5735e6ab3ac
Highbrook <Milivojevicdjole@yahoo.com>
2022-05-27 19:07:44 +0200

    Added invert functionality

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
+const html = document.querySelector('html');
 const cpOne = document.querySelector('#cpOne');
 const cpTwo = document.querySelector('#cpTwo');
-const html = document.querySelector('html');
+const invertEl = document.querySelector('#invert');
 
-cpOne.addEventListener('change', colorGrab);
+cpOne.addEventListener('change', colorChange);
+cpTwo.addEventListener('change', colorChange);
+invertEl.addEventListener('click', invertColors);
 
-cpTwo.addEventListener('change', colorGrab);
 
-function colorGrab() {
+function colorChange() {
     html.style.background = `linear-gradient(${cpOne.value}, ${cpTwo.value})`;
     console.log(cpOne.value, cpTwo.value);
 }
 
+function invertColors(){
+    cpTwoTemp = cpTwo.value;
+    cpTwo.value = cpOne.value;
+    cpOne.value = cpTwoTemp;
+    colorChange();
+}
