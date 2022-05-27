a48d72bdfcec6c9df3406e644bddf737881f17ba
Highbrook <Milivojevicdjole@yahoo.com>
2022-05-27 22:53:25 +0200

    added Conic gradient and CSS snippet

--- script.js
+++ script.js
@@ -2,20 +2,48 @@ const html = document.querySelector('html');
 const cpOne = document.querySelector('#cpOne');
 const cpTwo = document.querySelector('#cpTwo');
 const invertEl = document.querySelector('#invert');
+const radialEl = document.querySelector('#radial');
+const linearEl = document.querySelector('#linear');
+const conicEl =  document.querySelector('#conic');
+
+let cssValueEl = document.querySelector('#cssValue');
+let gradType = 'linear-gradient';
 
 cpOne.addEventListener('change', colorChange);
 cpTwo.addEventListener('change', colorChange);
 invertEl.addEventListener('click', invertColors);
+radialEl.addEventListener('click', radialGrad);
+linearEl.addEventListener('click', linearGrad);
+conicEl.addEventListener('click', conicGrad);
 
+function radialGrad() {
+    gradType = 'radial-gradient';
+    colorChange();
+}
+
+function linearGrad() {
+    gradType = 'linear-gradient';
+    colorChange();
+}
+
+function conicGrad() {
+    gradType = 'conic-gradient';
+    colorChange();
+}
 
 function colorChange() {
-    html.style.background = `linear-gradient(${cpOne.value}, ${cpTwo.value})`;
-    console.log(cpOne.value, cpTwo.value);
+    html.style.background = `${gradType}(${cpOne.value}, ${cpTwo.value})`;
+    cssSnippet();
+    console.log(cpOne.value, cpTwo.value, gradType);
 }
 
 function invertColors(){
     cpTwoTemp = cpTwo.value;
     cpTwo.value = cpOne.value;
     cpOne.value = cpTwoTemp;
-    colorChange();
+    colorChange(gradType);
+}
+
+function cssSnippet(){
+    cssValueEl.value = `background: ${html.style.background}`;
 }
